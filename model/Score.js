e347ece4cdcd8128f34ce41baf18ca449a5c73b6
awwgrima <guptaagrima200@gmail.com>
2022-09-11 14:53:11 +0530

    Score not duplicating

--- model/Score.js
+++ model/Score.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const scoreSchema = new Schema({
-    userID: String,
+    username: String,
     score: Number,
+    
     
 });
 
--- routes/scoreRoutes.js
+++ routes/scoreRoutes.js
@@ -3,27 +3,27 @@ const Score = mongoose.model('scores');
 
 module.exports = app => {
 app.get('/score' , async(req, res) => {
-    const { rUserID, rScore } = req.query;
-     if(rUserID == null || rScore == null)
+    const { rUserName, rScore } = req.query;
+     if(rUserName == null || rScore == null)
      {
         res.send("Invalid Credentials");
         return;
      }
-     var userScore = await Score.findOne({ userID: rUserID });
+     var userScore = await Score.findOne({ username: rUserName });
      if(userScore == null){
         console.log('Saving score...');
          var newAccount =new Score({
-            userID: rUserID,
+            username: rUserName,
             score : rScore,
          });
          await newAccount.save();
          res.send(newAccount);
          return;
      } else {
-            userScore.score = rScore;
-            await userScore.save();
-            res.send(userScore);
-            console.log('Updating data...');
+            // userScore.score = rScore;
+            // await userScore.save();
+            res.send(rScore);
+            console.log('Score already submitted');
             return;
      }
     //  res.send("Invalid Credentials");
