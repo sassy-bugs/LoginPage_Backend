1cbb34d61f56b9dd718373c2bce0e197aa29c4c0
awwgrima <guptaagrima200@gmail.com>
2022-09-26 15:12:22 +0530

    score not submitting(FIX NO. 2)

--- routes/scoreRoutes.js
+++ routes/scoreRoutes.js
@@ -9,13 +9,17 @@ app.get('/score' , async(req, res) => {
         res.send("Invalid Credentials");
         return;
      }
+     const date = new Date();
+     var ISToffSet = 330; //IST is 5:30; i.e. 60*5+30 = 330 in minutes 
+     offset= ISToffSet*60*1000;
+     var ISTTime = new Date(date.getTime()+offset);
      var userScore = await Score.findOne({ username: rUserName });
      if(userScore == null){
         console.log('Saving score...');
          var newAccount =new Score({
             username: rUserName,
             score : rScore,
-            lastAuthentication = (Date.getTime() + (330*60*1000))
+            lastAuthentication : ISTTime
                     
          });
          await newAccount.save();
