ed3baddc0d31edc53adb20876b4cb1656b028de5
Agrima Gupta <guptaagrima200@gmail.com>
2023-09-17 19:15:29 +0530

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,1 @@
-module.exports = {
-    port: process.env.PORT || 13756,
-    mongoURI: process.env.MONGO_URI || 'mongodb+srv://Magpie:0LkqPYkBV5768cOw@logindb.mhnthfv.mongodb.net/loginGameDB',
-};
+
